992c9bd805072a2304c90038e5dc9b33be2d754b
Lê Quốc Huy <huy78340@st.vimaru.edu.vn>
2022-04-24 22:02:44 +0700

    todo list

---
+++ todo-list/todo.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+
+function App() {
+  const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+
+  const [job,setJob] = useState('')
+  const [jobs,setJobs] = useState(storageJobs ?? [])
+
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJobs = [...prev,job]
+
+      const jsonJobs = JSON.stringify(newJobs)
+
+      localStorage.setItem('jobs',jsonJobs)
+      
+      return newJobs
+    })
+    setJob('')
+  }
+
+  return (
+    <div style={{ padding: 32 }}>
+      <input 
+        value={job} 
+        onChange={e => setJob(e.target.value)}/>
+      <button onClick={handleSubmit}>Add</button>
+
+      <ul>
+        {jobs.map((job,index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
